42d2fa7865741f25f8b422a1ce62bfa70ef10be6
dmitryt80 <79116243+dmitryt80@users.noreply.github.com>
2021-07-04 14:14:47 +0000

    Deploying to gh-pages from @ AleksandrYakovenko1984/team-project@5b10597929ba334f5156fd71e08099f8637be229 🚀

--- src.cf25617e.js
+++ src.cf25617e.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project/src.c1d364fe.js.map
+//# sourceMappingURL=/team-project/src.cf25617e.js.map
